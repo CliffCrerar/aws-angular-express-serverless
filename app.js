b211f2a27e0cff32432a839e3236cbf455fd87aa
Cliff Crerar <cliff@infinityarc.net>
2020-03-08 16:04:08 +0200

    adding updates

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
+app.get('/api', (req, res) => {
+  app.use(express.static('./www'));
+  res.status(200).set('content-type','text/html')
+  require('fs').ReadStream('./www/index.html').pipe(res)
 });
 
-app.post('/', function(req, res) {
+app.post('/api', function(req, res) {
   res.send({
     "Output": "Hello World!"
   });
